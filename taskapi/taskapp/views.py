1ad9c6f7697206dc365b104ecb63b53e681444f9
Abdulaziz Sayed <hanafiibrahim87@yahoo.com>
2023-02-25 19:20:09 +0200

    Added pagination to tasks endpoint

--- taskapi/taskapp/views.py
+++ taskapi/taskapp/views.py
@@ -1,12 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
 from . import  models, serailizers
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import LimitOffsetPagination
 
 
 class TaskViewSet(ModelViewSet):
     queryset = models.Task.objects.all()
     serializer_class = serailizers.TaskSerializer
     permission_classes = (IsAuthenticated,)
+    pagination_class = LimitOffsetPagination
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
