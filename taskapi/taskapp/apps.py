fb6447d62aaf6c1e4a441b8a15144b43676168ee
Abdulaziz Sayed <hanafiibrahim87@yahoo.com>
2023-02-25 18:34:20 +0200

    Task model created

--- taskapi/taskapp/apps.py
+++ taskapi/taskapp/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TaskappConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "taskapp"
+    name = "taskapi.taskapp"
--- taskapi/taskapp/models.py
+++ taskapi/taskapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    due_date = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    user = models.ForeignKey(User, related_name="tasks", on_delete=models.CASCADE)
