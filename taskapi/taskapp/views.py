fd7752ee3d3d31a3d1f384c092198dd615a15010
Abdulaziz Sayed <hanafiibrahim87@yahoo.com>
2023-02-25 19:15:06 +0200

    Task viewset created

--- taskapi/taskapp/views.py
+++ taskapi/taskapp/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from . import  models, serailizers
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class TaskViewSet(ModelViewSet):
+    queryset = models.Task.objects.all()
+    serializer_class = serailizers.TaskSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
